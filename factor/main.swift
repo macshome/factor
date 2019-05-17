af6168329ebf1c88d71931f349e7a9c179c18d68
Jayson Kish <kish.jayson@icloud.com>
2019-05-17 15:49:42 -0400

    Initial Commit

---
+++ factor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  factor
+//
+//  Created by Jayson Kish on 5/17/19.
+//  Copyright © 2019 ToplessBanana. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
